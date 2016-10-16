e830407d27b7efa87440081a18c0f815eb3783a6
Matthieu de Wit <Matt@matthieus-macbook-pro.local>
2016-10-16 14:03:36 +0200

    Added a new project called BluetoothSniffer

---
+++ BluetoothSniffer/BluetoothSniffer/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  BluetoothSniffer
+//
+//  Created by Matthieu de Wit on 16-10-16.
+//  Copyright © 2016 MdW Development. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
